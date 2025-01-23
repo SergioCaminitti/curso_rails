682d4cacc6b07cab9cb57f17613d2ed4378f604b
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-23 14:41:00 -0300

    Finalizando aula 10 Compreendendo o ERB

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "home", controller: "home", action: "index"
   get "home2", to: "home#index" 
-
 end
