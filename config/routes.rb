15353fb565207a7cbb54054706005845318bd673
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-23 15:08:01 -0300

    Finalizando Exercício 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "home", controller: "home", action: "index"
-  get "home2", to: "home#index" 
+  get "estudantes", to: "estudante#index"
 end
