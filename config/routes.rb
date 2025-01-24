2e91e35196d97fd387c76082dd0da33257085b34
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-24 11:28:05 -0300

    Implementando a navbar e estilizando ela para a aplicação

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "inicio", controller: "home", action: "index", as: "home"
   get "estudante", to: "estudante#index", as: "estudantes"
-
   root "home#index"
 end
