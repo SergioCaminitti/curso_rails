756e1dbe8fdae42c8d61a62c8be02b4411f6ed25
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-24 11:09:17 -0300

    Importando Bootstrap para a aplicação e atualizando a lista de informações do estudante

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  get "home", controller: "home", action: "index"
-  get "estudantes", to: "estudante#index"
+  get "inicio", controller: "home", action: "index", as: "home"
+  get "estudante", to: "estudante#index", as: "estudantes"
+
+  root "home#index"
 end
