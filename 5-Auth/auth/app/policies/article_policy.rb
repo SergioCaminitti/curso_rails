86acac318a99e00c33b64e62f49a00108681ca60
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-28 18:08:34 -0300

    Definindo Políticas de Acesso

---
+++ 5-Auth/auth/app/policies/article_policy.rb
@@ -0,0 +1,22 @@
+class ArticlePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+    def update?
+      # se é admin
+      # user&.admin?
+      # se é dono do artigo
+      user&.admin? || user&.id == record.user_id
+    end
+
+    def destroy?
+      # se é admin
+      # user&.admin?
+      # se é dono do artigo
+      user&.admin? || user&.id == record.user_id
+    end
+end
