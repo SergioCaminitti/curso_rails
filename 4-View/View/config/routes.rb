8a821beb084ec0df7e3c9e93eb71b455ec6b0870
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-28 10:51:18 -0300

    Aula de Assets e Helper

--- 4-View/View/config/routes.rb
+++ 4-View/View/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get "usuarios/confirmacao"
   get "exercicio1/index"
   # get "test", to: "welcome#index"
-  # root "welcome#index"
-  root "exercicio1#index"
+  root "welcome#index"
+  # root "exercicio1#index"
   post "adicionar_usuario", to: "exercicio1#adicionar_usuario"
 
   post "adicionar_produto", to: "welcome#adicionar_produto"
