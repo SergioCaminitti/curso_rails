703590aeb7692b3aabc4aab7b02268fd289d1966
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-30 11:29:58 -0300

    Shared Examples

---
+++ 7-Testes/spec/game_shared_spec.rb
@@ -0,0 +1,39 @@
+require 'models/game'
+
+RSpec.shared_examples 'a PS5 game' do
+  let(:game) {@game}
+
+  it 'should have a title' do
+    expect(@game.title).to be_truthy
+  end
+  it 'shoul allow settings and updating the title' do
+    @game.title = 'Demon\'s Souls'
+    expect(@game.title).to eq('Demon\'s Souls')
+    @game.title = 'Ratchet & Clank: Rift Apart'
+    expect(@game.title).to eq('Ratchet & Clank: Rift Apart')
+  end
+  it 'should have a release year within a specif range' do
+    expect(@game.release_year).to be_within(1).of(2020)
+  end
+  it 'should include specific features' do
+    expect(@game.features).to include('Open World')
+    expect(@game.features).to include('Action-Packed')
+  end
+end
+
+RSpec.describe 'Game' do
+  before(:each) do
+    @game = Game.new('The Legend of Zelda: Breath of the Wild', 2020, ['Open World', 'Action-Packed'])
+  end
+
+  context 'when dealing with PS5 games' do
+    include_examples 'a PS5 game', @game
+  end
+
+  context 'role-playing games (RPGs)' do
+    before(:each) do
+      @rpg_game = Game.new('Final Fantasy VII Remake', 2020, ['Story-Rich', 'Turn-Based'])
+    end
+    include_examples 'a PS5 game', @rpg_game
+  end
+end
--- 7-Testes/spec/game_spec.rb
+++ 7-Testes/spec/game_spec.rb
@@ -37,4 +37,22 @@ RSpec.describe 'Game' do
       # verifica se um elemento está presente em um array
     end
   end
+
+  context 'role-playing games (RPGs)' do
+    before(:each) do
+      @rpg_game = Game.new('Final Fantasy VII Remake', 2020, ['Story-Rich', 'Turn-Based'])
+    end
+
+    it 'should have a title' do
+      expect(@rpg_game.title).to be_truthy
+    end
+    it 'should have a release year within a specif range' do
+      expect(@rpg_game.release_year).to be_within(1).of(2020)
+    end
+    it 'should include specific features' do
+      expect(@rpg_game.features).to include('Story-Rich')
+      expect(@rpg_game.features).to include('Turn-Based')
+    end
+
+  end
 end
