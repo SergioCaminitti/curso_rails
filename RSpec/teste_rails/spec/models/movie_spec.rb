2b9eb5506f0d9bc4d9c26d6db3e1ba5405c61d85
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-30 14:59:31 -0300

    Primeiro Caso de Teste

--- RSpec/teste_rails/spec/models/movie_spec.rb
+++ RSpec/teste_rails/spec/models/movie_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with attributs' do
+    movie = Movie.new(
+     title: 'Movie Title',
+     description: 'Movie Description',
+     director: 'Movie Director',
+     duration: 120
+    )
+    expect(movie).to be_valid
+  end
 end
