69c5c35460f1a277523e4d776ae9f7bd812b10aa
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-30 15:07:26 -0300

    Validando a Duração dos Filmes

--- RSpec/teste_rails/app/models/movie.rb
+++ RSpec/teste_rails/app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :duration, numericality: {greather_than: 0}
+
+  #método para calcular a duração dos filmes
+  def self.average_duration
+    total_duration = Movie.sum(:duration)
+    total_movies = Movie.count
+    return nil if total_movies.zero?
+    total_duration / total_movies
+  end
 end
--- RSpec/teste_rails/spec/models/movie_spec.rb
+++ RSpec/teste_rails/spec/models/movie_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Movie, type: :model do
     )
     expect(movie).to be_valid
   end
+
+  context "when there are movies with durations" do
+    it "retur the correct average duration" do
+      #criar alguns filmes com diferentes durações
+      Movie.create(title: "Movie 1", duration: 90)
+      Movie.create(title: "Movie 2", duration: 120)
+      #calcular a média da duração
+      average_duration = Movie.average_duration
+      #verificar se a média da duração é calculada corretamente
+      expect(average_duration).to eq(105)
+    end
+  end
 end
