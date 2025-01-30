75e87682037bbbbb92744c74e18e3ee89f6ce7c3
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-30 10:55:11 -0300

    Refatorando o Projeto

--- 7-Testes/spec/game_spec.rb
+++ 7-Testes/spec/game_spec.rb
@@ -4,19 +4,20 @@
 # Expect: Usado para comparar o resultado esperado com o resultado obtido
 # Eq: Usado para comparar o resultado esperado com o resultado obtido
 
-class Game
-  attr_accessor :title
-
-  def initialize(title)
-    @title = title
-  end
-end
+require 'models/game'
 
 RSpec.describe 'Game' do
   context 'when dealing with PS5 games' do
     it 'should have a title' do
       game = Game.new('Spider-Man: Miles Morales')
       expect(game.title).to eq('Spider-Man: Miles Morales')
+    end
+    it 'shoul allow settings and updating the title' do
+      game2 = Game.new('Demon\'s Souls')
+      expect(game2.title).to eq('Demon\'s Souls')
+
+      game3 = Game.new('Ratchet & Clank: Rift Apart')
+      expect(game3.title).to eq('Ratchet & Clank: Rift Apart')
     end
   end
 end
