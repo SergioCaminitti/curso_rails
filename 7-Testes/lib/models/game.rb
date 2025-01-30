de8e2a5ed52c6f7c059b95172030d3a2ac622581
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-30 11:15:38 -0300

    Utilizando Matchers

--- 7-Testes/lib/models/game.rb
+++ 7-Testes/lib/models/game.rb
@@ -1,7 +1,9 @@
 class Game
-  attr_accessor :title
+  attr_accessor :title, :release_year, :features
 
-  def initialize(title)
+  def initialize(title, release_year = nil, features = [])
     @title = title
+    @release_year = release_year
+    @features = features
   end
 end
--- 7-Testes/spec/game_spec.rb
+++ 7-Testes/spec/game_spec.rb
@@ -13,19 +13,28 @@ require 'models/game'
 
 RSpec.describe 'Game' do
   before(:each) do
-    @game = Game.new('The Legend of Zelda: Breath of the Wild')
+    @game = Game.new('The Legend of Zelda: Breath of the Wild', 2020, ['Open World', 'Action-Packed'])
   end
 
   context 'when dealing with PS5 games' do
     it 'should have a title' do
-      expect(@game.title).to eq('The Legend of Zelda: Breath of the Wild')
+      # expect(@game.title).to eq('The Legend of Zelda: Breath of the Wild')
+      expect(@game.title).to be_truthy
+      # verifica se uma condição é verdadeira ou falsa
     end
     it 'shoul allow settings and updating the title' do
       @game.title = 'Demon\'s Souls'
       expect(@game.title).to eq('Demon\'s Souls')
-
       @game.title = 'Ratchet & Clank: Rift Apart'
       expect(@game.title).to eq('Ratchet & Clank: Rift Apart')
+    end
+    it 'should have a release year within a specif range' do
+      expect(@game.release_year).to be_within(1).of(2020)
+    end
+    it 'should include specific features' do
+      expect(@game.features).to include('Open World')
+      expect(@game.features).to include('Action-Packed')
+      # verifica se um elemento está presente em um array
     end
   end
 end
